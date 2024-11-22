e6f40289de96eafa345a5a3743691b71995a75df
Joao-Machado-UAb <1300213@estudante.uab.pt>
2024-11-22 21:28:11 +0000

    Update app.py

--- app.py
+++ app.py
@@ -6,11 +6,11 @@ app = Flask(__name__)
 # Dados de exemplo para os pedidos da Inven!RA
 data = {
     "name": "Exercícios de equações do 1.º grau a uma incógnita",
-    "config_url": "http://<domínio>/configuracao-atividade.html",
-    "json_params_url": "http://<domínio>/json-params-atividade",
-    "user_url": "http://<domínio>/deploy-atividade",
-    "analytics_url": "http://<domínio>/analytics-atividade",
-    "analytics_list_url": "http://<domínio>/lista-analytics-atividade"
+    "config_url": "https://edumat7.onrender.com/configuracao-atividade.html",
+    "json_params_url": "https://edumat7.onrender.com/json-params-atividade",
+    "user_url": "https://edumat7.onrender.com/deploy-atividade",
+    "analytics_url": "https://edumat7.onrender.com/analytics-atividade",
+    "analytics_list_url": "https://edumat7.onrender.com/lista-analytics-atividade"
 }
 
 @app.route("/")
