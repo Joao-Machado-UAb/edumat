8c28d70da2e65d390cf31628dbed8e6c60795b95
Joao-Machado-UAb <1300213@estudante.uab.pt>
2024-12-07 15:40:02 +0000

    Update singleton_db.py

--- singleton_db.py
+++ singleton_db.py
@@ -14,14 +14,17 @@ class SingletonDB:
 
     def create_instance(self, activity_id):
         if activity_id not in self._instance.db:
-            self._instance.db[activity_id] = {"resumo": "", "instrucoes": ""}
+            self._instance.db[activity_id] = {
+                "resumo": "Resumo de equações de 7º ano: Aqui você pode encontrar um resumo das equações de 7º ano.",
+                "link_questoes": "https://exemplo.com/questoes_equacoes_7_ano"
+            }
         return self._instance.db[activity_id]
 
     def access_data(self, activity_id):
         return self._instance.db.get(activity_id, None)
 
-    def execute_operations(self, activity_id, resumo, instrucoes):
+    def execute_operations(self, activity_id, resumo, link_questoes):
         if activity_id in self._instance.db:
             self._instance.db[activity_id]["resumo"] = resumo
-            self._instance.db[activity_id]["instrucoes"] = instrucoes
+            self._instance.db[activity_id]["link_questoes"] = link_questoes
         return self._instance.db[activity_id]
