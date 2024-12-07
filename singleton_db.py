9b26e50d13e42ec01100622642a8d0a7daa8e989
Joao-Machado-UAb <1300213@estudante.uab.pt>
2024-12-07 17:46:28 +0000

    Update singleton_db.py

--- singleton_db.py
+++ singleton_db.py
@@ -2,16 +2,16 @@
 
 class SingletonDB:
     _instance = None
-
+    
     def __new__(cls, *args, **kwargs):
         if not cls._instance:
-            cls._instance = super(SingletonDB, cls).__new__(cls, *args, **kwargs)
+            cls._instance = super(SingletonDB, cls).__new__(cls)
             cls._instance.db = {}  # Simula um banco de dados
         return cls._instance
-
+    
     def get_instance(self):
         return self._instance.db
-
+    
     def create_instance(self, activity_id):
         if activity_id not in self._instance.db:
             self._instance.db[activity_id] = {
@@ -19,10 +19,10 @@ class SingletonDB:
                 "instrucoes": "https://mundoeducacao.uol.com.br/matematica/quatro-passos-para-resolver-equacoes-primeiro-grau.htm"
             }
         return self._instance.db[activity_id]
-
+    
     def access_data(self, activity_id):
         return self._instance.db.get(activity_id, None)
-
+    
     def execute_operations(self, activity_id, resumo, instrucoes):
         if activity_id in self._instance.db:
             self._instance.db[activity_id]["resumo"] = resumo
