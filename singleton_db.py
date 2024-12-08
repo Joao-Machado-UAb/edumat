c2e45c4d7b622140a05ed51381924c3f16edd7f0
Joao-Machado-UAb <1300213@estudante.uab.pt>
2024-12-08 11:48:57 +0000

    Update singleton_db.py
    Resumo retirado da class

--- singleton_db.py
+++ singleton_db.py
@@ -1,5 +1,7 @@
 # singleton_db.py
 
+DEFAULT_RESUMO = "Resumo de equações de 7º ano: Aqui você pode encontrar um resumo das equações de 7º ano."
+
 
 class SingletonDB:
     _instance = None
@@ -7,7 +9,7 @@ class SingletonDB:
     def __new__(cls, *args, **kwargs):
         if not cls._instance:
             cls._instance = super(SingletonDB, cls).__new__(cls)
-            cls._instance.db = {}  # Simula um banco de dados
+            cls._instance.db = {}
         return cls._instance
 
     def get_instance(self):
@@ -16,7 +18,7 @@ class SingletonDB:
     def create_instance(self, activity_id):
         if activity_id not in self._instance.db:
             self._instance.db[activity_id] = {
-                "resumo": "Resumo de equações de 7º ano: Aqui você pode encontrar um resumo das equações de 7º ano.",
+                "resumo": DEFAULT_RESUMO,
                 "instrucoes": "https://www.matematica.pt/aulas-matematica.php?ano=7",
             }
         return self._instance.db[activity_id]
