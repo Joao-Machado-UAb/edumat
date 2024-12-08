9edf0d65fb29b9baed73cadaaf24f921fee08d20
Joao-Machado-UAb <1300213@estudante.uab.pt>
2024-12-08 16:12:05 +0000

    Update singleton_db.py
    anotações

--- singleton_db.py
+++ singleton_db.py
@@ -4,6 +4,8 @@ DEFAULT_RESUMO = "Resumo de equações de 7º ano: Aqui você pode encontrar um 
 
 
 class SingletonDB:
+    # Classe Singleton para gerenciar um banco de dados
+
     _instance = None
 
     def __new__(cls, *args, **kwargs):
@@ -12,10 +14,12 @@ class SingletonDB:
             cls._instance.db = {}
         return cls._instance
 
-    def get_instance(self):
+    def get_database(self):
+        # Retorna o banco de dados
         return self._instance.db
 
     def create_instance(self, activity_id):
+        # Cria uma entrada no banco de dados se ela não existir
         if activity_id not in self._instance.db:
             self._instance.db[activity_id] = {
                 "resumo": DEFAULT_RESUMO,
@@ -24,10 +28,24 @@ class SingletonDB:
         return self._instance.db[activity_id]
 
     def access_data(self, activity_id):
+        # Retorna os dados de uma entrada específica ou None se não existir
         return self._instance.db.get(activity_id, None)
 
-    def execute_operations(self, activity_id, resumo, instrucoes):
-        if activity_id in self._instance.db:
+    def execute_operations(self, activity_id, resumo=None, instrucoes=None):
+        """
+        Atualiza os dados de uma entrada existente no banco de dados.
+
+        Parâmetros:
+            activity_id: ID da atividade a ser atualizada
+            resumo: Novo resumo (opcional).
+            instrucoes: Novas instruções (opcional).
+        """
+        if activity_id not in self._instance.db:
+            raise KeyError(
+                f"Activity ID '{activity_id}' não encontrado no banco de dados."
+            )
+        if resumo:
             self._instance.db[activity_id]["resumo"] = resumo
+        if instrucoes:
             self._instance.db[activity_id]["instrucoes"] = instrucoes
         return self._instance.db[activity_id]
