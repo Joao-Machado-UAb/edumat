06f59196fd67139dd88fa064450ec5c374a80f72
Joao-Machado-UAb <1300213@estudante.uab.pt>
2025-01-26 17:09:22 +0000

    Update activity_facade.py

--- activity_facade.py
+++ activity_facade.py
@@ -1,44 +1,43 @@
 # activity_facade.py
 
-from singleton_db import SingletonDB
-from observers import (
-    ActivityAnalytics,
-    QualitativeAnalyticsObserver,
-    QuantitativeAnalyticsObserver,
-)
-
+from singleton_db import DatabaseService
+from observers import ActivityAnalytics, QualitativeAnalyticsObserver, QuantitativeAnalyticsObserver
 
 class ActivityFacade:
     def __init__(self):
-        self.db_service = SingletonDB()
+        self.db_service = DatabaseService()
         self.analytics = ActivityAnalytics(self.db_service)
-
+        
         # Anexar observadores
         self.analytics.attach(QualitativeAnalyticsObserver(self.db_service))
         self.analytics.attach(QuantitativeAnalyticsObserver(self.db_service))
 
     def create_activity(self, activity_id: str):
         activity = self.db_service.create_activity(activity_id)
-        self.analytics.notify(activity_id, "system", {"acesso_atividade": True})
+        self.analytics.notify(
+            activity_id, 
+            'system', 
+            {'acesso_atividade': True}
+        )
         return activity
 
-    def update_activity(
-        self, activity_id: str, resumo: str = None, instrucoes: str = None
-    ):
-        updated_activity = self.db_service.update_activity(
-            activity_id, resumo, instrucoes
+    def update_activity(self, activity_id: str, resumo: str = None, instrucoes: str = None):
+        updated_activity = self.db_service.update_activity(activity_id, resumo, instrucoes)
+        self.analytics.notify(
+            activity_id, 
+            'system', 
+            {'atividade_atualizada': True}
         )
-        self.analytics.notify(activity_id, "system", {"atividade_atualizada": True})
         return updated_activity
 
     def get_analytics_list(self):
         return {
             "qualAnalytics": [
                 {"name": "Acesso à atividade", "type": "boolean"},
-                {"name": "Download de recursos", "type": "boolean"},
+                {"name": "Download de recursos", "type": "boolean"}
             ],
             "quantAnalytics": [
                 {"name": "Número de acessos", "type": "integer"},
-                {"name": "Progresso na atividade", "type": "percentage"},
-            ],
+                {"name": "Progresso na atividade", "type": "percentage"}
+            ]
         }
