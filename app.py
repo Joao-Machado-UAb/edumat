ea5bd90d09a79798d6bc04e81e983ae074b23afd
Joao-Machado-UAb <1300213@estudante.uab.pt>
2025-01-30 19:28:53 +0000

    Update app.py

--- app.py
+++ app.py
@@ -1,5 +1,4 @@
 # app.py
-
 from flask import Flask, jsonify, request, render_template
 from activity_facade import ActivityFacade
 
@@ -23,10 +22,12 @@ def config():
 # parametros json
 @app.route("/json_params", methods=["GET"])
 def json_params():
-    return jsonify([
-        {"name": "resumo", "type": "text/plain"},
-        {"name": "instrucoes", "type": "text/plain"},
-    ])
+    return jsonify(
+        [
+            {"name": "resumo", "type": "text/plain"},
+            {"name": "instrucoes", "type": "text/plain"},
+        ]
+    )
 
 
 # Lista de analytics da atividade
@@ -53,11 +54,14 @@ def deploy():
     resumo = json_params.get("resumo", "")
     instrucoes = json_params.get("instrucoes", "")
     activity_facade.update_activity(activity_id, resumo, instrucoes)
-    return jsonify({
-        "url": f"https://edumat.onrender.com/atividade?id={activity_id}&student_id={student_id}"
-    })
+    return jsonify(
+        {
+            "url": f"https://edumat.onrender.com/atividade?id={activity_id}&student_id={student_id}"
+        }
+    )
 
 
+# analytics da atividade
 @app.route("/analytics", methods=["GET"])
 def analytics():
     analytics_data = activity_facade.get_analytics_data()
